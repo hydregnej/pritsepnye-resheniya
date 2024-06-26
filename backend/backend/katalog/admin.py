30a7cb43793a90f8854615a6ba381d5025b02eda
hydregnej <78560174+hydregnej@users.noreply.github.com>
2024-06-26 19:31:05 +0500

    Перенёс backend в ветку backend

--- backend/backend/katalog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Katalog, Order, Sub, News
-
-admin.site.register(Katalog)
-admin.site.register(Order)
-admin.site.register(Sub)
-admin.site.register(News)
