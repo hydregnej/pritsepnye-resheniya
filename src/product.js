dec8e686a32698966db0bf904ece94cc5f2602f9
hydregnej <78560174+hydregnej@users.noreply.github.com>
2024-06-26 19:19:31 +0500

    Оставил только backend

--- src/product.js
+++
@@ -1,7 +0,0 @@
-import "./styles/header.css";
-import "./styles/footer.css";
-import "./styles/reset.css";
-import "./styles/fonts.css";
-import "./styles/moving-button.css";
-
-import "./components/header.js";
