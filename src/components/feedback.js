ba01ce434c768cc64f2e4f62300373a858bb0248
hydregnej <78560174+hydregnej@users.noreply.github.com>
2024-07-02 13:16:10 +0500

    добавлена валидация и форматирование номера телефона
    - Добавлена логика для автоматического добавления +7 в начале номера телефона
    - Добавлены скобки и дефисы для форматирования номера в виде +7(999)999-99-99
    - Реализовано удаление первых цифр, если они равны 7 или 8, чтобы избежать дублирования +7
    - Обработчики событий предотвращают ввод нецифровых символов

--- src/components/feedback.js
+++ src/components/feedback.js
@@ -50,3 +50,55 @@ document.querySelector(".feedback__form").addEventListener("submit", async funct
     alert('Произошла ошибка при отправке формы. Пожалуйста, попробуйте еще раз.');
   }
 });
+
+
+// Ограничение ввода символом и ввод начинается с +7
+document.addEventListener('DOMContentLoaded', () => {
+  const phoneInput = document.getElementById('phone')
+  
+  phoneInput.addEventListener('input', (event) => {
+    let value = phoneInput.value
+    
+    // Удаление всех символов кроме цифр
+    value = value.replace(/\D/g, '')
+
+    // Если первая цифра 8, заменяем ее на 7
+    if (value.startsWith('8')) {
+      value = value.substring(1)
+    }
+
+    // Если начинается не с 7, удаляем первую цифру
+    if (value.startsWith('7')) {
+      value = value.substring(1)
+    }
+    
+    // Ограничение длины до 10 символов (без учета +7)
+    if (value.length > 10) {
+      value = value.substring(0, 10)
+    }
+
+    // Форматирование номера телефона в виде +7(999)999-99-99
+    let formattedValue = '+7'
+    if (value.length > 0) {
+      formattedValue += '(' + value.substring(0, 3)
+    }
+    if (value.length >= 4) {
+      formattedValue += ')' + value.substring(3, 6)
+    }
+    if (value.length >= 7) {
+      formattedValue += '-' + value.substring(6, 8)
+    }
+    if (value.length >= 9) {
+      formattedValue += '-' + value.substring(8, 10)
+    }
+
+    phoneInput.value = formattedValue
+  })
+  
+  phoneInput.addEventListener('keypress', (event) => {
+    // Разрешение ввода только цифр
+    if (!/[0-9]/.test(event.key)) {
+      event.preventDefault()
+    }
+  })
+})
