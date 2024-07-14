4497af25e27dae89320ae6bc4ffc7234d5a4a1f8
hydregnej <78560174+hydregnej@users.noreply.github.com>
2024-07-14 22:45:21 +0500

    Добавил адаптив к "Контактам"

--- src/contacts.js
+++ src/contacts.js
@@ -3,6 +3,10 @@ import "./styles/header.css";
 import "./styles/footer.css";
 import "./styles/fonts.css";
 import "./styles/moving-button.css";
+import "./styles/breadcrumbs.css";
+import "./styles/main-feedback-form.css";
+import "./styles/contacts.css";
 
 import "./components/header.js";
 import "./components/map.js";
+import "./components/feedback.js";
