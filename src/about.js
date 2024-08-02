f65ef52f3d5def031b280a1473310a2c40b31be3
hydregnej <78560174+hydregnej@users.noreply.github.com>
2024-08-02 12:17:14 +0500

    restart files

--- src/about.js
+++
@@ -1,8 +0,0 @@
-import "./components/reset/reset.css";
-import "./components/shrifts/fonts.css";
-import "./components/floating-buttons/moving-button.css";
-import "./components/header/header.css";
-import "./components/footer/footer.css";
-import "./components/about-us/about.css";
-
-import "./components/header/header.js";
